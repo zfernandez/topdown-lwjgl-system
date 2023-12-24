a706d25c0253cb4fe532648e0d9896887f168459
zfernandez <zfernandez2010@gmail.com>
2023-12-24 14:29:33 -0600

    Partial #5 Adding Scene items
    Problems found with a println in the LevelEditorScene, pushing to find the diff

---
+++ src/main/java/jade/LevelScene.java
@@ -0,0 +1,15 @@
+package jade;
+
+public class LevelScene extends Scene {
+    public LevelScene() {
+        System.out.println("Inside level scene");
+        Window.get().r = 1.0f;
+        Window.get().g = 1.0f;
+        Window.get().b = 1.0f;
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
